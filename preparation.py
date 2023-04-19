81ef097c13088efa0e52f316b10c3abcabd4d7c4
Diego Juarez <diego.juarez@student.howest.be>
2023-04-19 15:04:27 -0600

    Plagiarism Detection 1 v 1
    Co-authored-by: luis-zamarripa <luis-zamarripa@users.noreply.github.com>
    Co-authored-by: Erick Bustos <erteck@users.noreply.github.com>

--- preparation.py
+++ preparation.py
@@ -1,5 +1,8 @@
 from preprocess import preprocessing
 from nltk.util import ngrams
+from sklearn.metrics import pairwise
+from difflib import SequenceMatcher
+
 
 def create_n_grams(p1,p2,n):
     ngrams1 = ngrams(p1, n)
@@ -20,6 +23,25 @@ def build_embeddings(embeddings,uniques,paragraphs):
             if s in paragraphs[p]:
                 embeddings[p][i] = 1
 
+def find_similarities(text1, text2):
+    # Reading files
+    with open(text1, "r") as legit:
+        legit = legit.read()
+    with open(text2, "r") as plagiarized:
+        plagiarized = plagiarized.read()
+    
+    matcher = SequenceMatcher(None, legit, plagiarized)
+    matching_blocks = matcher.get_matching_blocks()
+    similarities = []
+    for block in matching_blocks:
+        start = block.a
+        end = block.a + block.size
+        similarity = legit[start:end]
+        position = block.b
+        similarities.append((similarity, position))
+
+    return [similarity for similarity in similarities if len(similarity[0]) > 10]
+
 def preparation():
     text1 = "legit.txt"
     text2 = "plagiarized.txt"
@@ -38,10 +60,24 @@ def preparation():
     build_embeddings(embeddings_uni, unigrams, paragraphs)
     build_embeddings(embeddings_tri, trigrams, paragraphs)
 
-    print(unigrams)
-    # print(trigrams)
-    print(embeddings_uni)
-    # print(embeddings_tri)
+    
+    # Cosine similarity para unigramas
+    unigram_result = pairwise.cosine_similarity(embeddings_uni)[0,1]
+    print(unigram_result)
+    trigram_result = pairwise.cosine_similarity(embeddings_tri)[0,1]
+    print(trigram_result)
+
+    pointers = False
+    if unigram_result > 0.5 and trigram_result > 0.1:
+        print("Plagiarism detected")
+        pointers = True
+        print("⚠️ ⚠️ ⚠️ ⚠️ ⚠️ ⚠️")
+    
+    if pointers:
+        similarities = find_similarities(text1, text2)
+        for similarity, position in similarities:
+            print(f"Similarity: '{similarity}', Position: {position}")\
+    
     
 
 if __name__ == "__main__":
